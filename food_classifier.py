054029653931a601b75c59ea732108cf05a56f1c
Apoorav Rathore <apoorav.rathore@icloud.com>
2025-04-26 18:35:19 -0500

    wrote food classification part; model is very *very* light for testing

---
+++ food_classifier.py
@@ -0,0 +1,42 @@
+import torch
+from torchvision import transforms, models
+from PIL import Image
+import os
+
+from datasets import load_dataset
+
+# load trained model
+model_path = "./models/food101_resnet18.pth"
+model = models.resnet18(pretrained=False)
+
+NUM_CLASSES = 101 # food 101
+model.fc = torch.nn.Linear(model.fc.in_features, NUM_CLASSES)
+
+# load saved weights
+model.load_state_dict(torch.load("./models/food101_resnet18.pth"))
+model.eval()
+
+# define transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[.229, .224, .225]) # ImageNet
+])
+
+# load and preprocess image
+image_path = input("enter image path: ")
+image = Image.open(image_path).convert("RGB")
+image = transform(image).unsqueeze(0)
+
+# predict
+with torch.no_grad():
+    outputs = model(image)
+    logits = outputs
+    predicted_class_idx = logits.argmax(dim=1).item()
+
+dataset = load_dataset("food101", split="train")
+food_labels = dataset.features['label'].names
+
+predicted_label = food_labels[predicted_class_idx]
+
+print(f"predicted food: {predicted_label}")
