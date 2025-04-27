fbf8a4df1b28b808220c6a0df6bd519dd1528382
Apoorav Rathore <apoorav.rathore@icloud.com>
2025-04-26 19:19:39 -0500

    basic project working, model needs improvement

---
+++ calorie_estimator.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from food_classifier import classify_food
+
+from fuzzywuzzy import fuzz
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# load calories data
+calories_df = pd.read_csv('data/calories.csv')
+
+# estimate calories based on food prediction
+def estimate_calories(predicted_food, weight_in_grams):
+    # find food in dataframe
+    food_row = calories_df[calories_df['FoodItem'] == predicted_food]
+
+    if not food_row.empty:
+        # get calories per 100 grams
+        cals_per_100g = food_row['Cals_per100grams'].values[0]
+
+        # calculate cals baesd on weight
+        estimated_calories = (cals_per_100g * weight_in_grams) / 100
+        return estimated_calories
+    else:
+        return None # predicted category not in csv
+
+# use fuzzing to get the closest match in the data if we don't have an exact match
+def get_closest_food(predicted_food):
+    closest_match = max(calories_df['FoodItem'], key=lambda x:fuzz.ratio(predicted_food.lower(), x.lower()))
+
+    return closest_match
+    
+def classify_and_estimate(image_path, weight_in_grams=100):
+    # predict which food
+    predicted_food = classify_food(image_path)
+
+    # estimate calories
+    calories = estimate_calories(predicted_food, weight_in_grams)
+
+    # if exact match  not found in data
+    if calories is None:
+        # get closest match
+        closest_food = get_closest_food(predicted_food)
+        print(f"predicted food: '{predicted_food}' not found. using closest match: {closest_food}")
+        calories = estimate_calories(closest_food, weight_in_grams)
+    
+    if calories is not None:
+        print(f"predicted food: {predicted_food}")
+        print(f"estimated calories for {weight_in_grams} grams: {calories:.2f} kcal")
+    else:
+        print(f"unable to estimate calories for {predicted_food} or closest match")
+
+image_path = input("enter image path: ")
+food_weight = int(input("enter food weight (grams): "))
+
+classify_and_estimate(image_path, weight_in_grams=food_weight)
+        
--- food_classifier.py
+++ food_classifier.py
@@ -5,38 +5,42 @@ import os
 
 from datasets import load_dataset
 
-# load trained model
-model_path = "./models/food101_resnet18.pth"
-model = models.resnet18(pretrained=False)
+def classify_food(image_path):
+    # load trained model
+    model_path = "./models/food101_resnet18.pth"
+    model = models.resnet18(weights=None)
 
-NUM_CLASSES = 101 # food 101
-model.fc = torch.nn.Linear(model.fc.in_features, NUM_CLASSES)
+    NUM_CLASSES = 101 # food 101
+    model.fc = torch.nn.Linear(model.fc.in_features, NUM_CLASSES)
 
-# load saved weights
-model.load_state_dict(torch.load("./models/food101_resnet18.pth"))
-model.eval()
+    # load saved weights
+    model.load_state_dict(torch.load("./models/food101_resnet18.pth"))
+    model.eval()
 
-# define transform
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[.229, .224, .225]) # ImageNet
-])
+    # define transform
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[.229, .224, .225]) # ImageNet
+    ])
 
-# load and preprocess image
-image_path = input("enter image path: ")
-image = Image.open(image_path).convert("RGB")
-image = transform(image).unsqueeze(0)
+    # load and preprocess image
+    image = Image.open(image_path).convert("RGB")
+    image = transform(image).unsqueeze(0)
 
-# predict
-with torch.no_grad():
-    outputs = model(image)
-    logits = outputs
-    predicted_class_idx = logits.argmax(dim=1).item()
+    # predict
+    with torch.no_grad():
+        outputs = model(image)
+        logits = outputs
+        predicted_class_idx = logits.argmax(dim=1).item()
 
-dataset = load_dataset("food101", split="train")
-food_labels = dataset.features['label'].names
+    dataset = load_dataset("food101", split="train")
+    food_labels = dataset.features['label'].names
 
-predicted_label = food_labels[predicted_class_idx]
+    predicted_label = food_labels[predicted_class_idx]
 
-print(f"predicted food: {predicted_label}")
+    #print(f"predicted food: {predicted_label}")
+    return predicted_label
+
+#path = input("enter image path: ")
+#classify_food(path)
